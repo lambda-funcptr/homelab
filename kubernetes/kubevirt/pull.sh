5d90788e3295e6d6e1f34f58bdd1f490a118db4d
Guanyu Wu <lambda@funcptr.org>
2020-07-31 15:58:36 -0400

    The kubevirt CDI to the kubevirt pull script.

--- kubernetes/kubevirt/pull.sh
+++ kubernetes/kubevirt/pull.sh
@@ -8,10 +8,12 @@ rm -r kubevirt-dist
 mkdir kubevirt-dist
 cd kubevirt-dist
 
+# Pull kubevirt itself.
 wget https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERS}/kubevirt-operator.yaml -O 00_kubevirt-operator.yaml
 wget https://github.com/kubevirt/kubevirt/releases/download/${KUBEVIRT_VERS}/kubevirt-cr.yaml -O 01_kubevirt-cr.yaml
 wget https://raw.githubusercontent.com/kubevirt/kubevirt/master/LICENSE -O LICENSE.kubevirt
 
+# Pull kubevirt-CDI for image imports.
 CDI_VERSION=$(curl -s https://github.com/kubevirt/containerized-data-importer/releases/latest | grep -o "v[0-9]\.[0-9]*\.[0-9]*")
 wget https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator.yaml -O 10_cdi-operator.yaml
 wget https://github.com/kubevirt/containerized-data-importer/releases/download/$CDI_VERSION/cdi-operator-cr.yaml -O 11_cdi-operator-cr.yaml
