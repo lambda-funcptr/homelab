6051db1a8f900409549e8e232ea8aa8591acefed
Guanyu Wu <lambda@funcptr.org>
2020-08-06 22:11:08 -0400

    Preliminary grafana config. Still doesn't persist data yet. I'm getting there.

--- secrets/_upload-secrets.sh
+++ secrets/_upload-secrets.sh
@@ -2,4 +2,10 @@
 
 cd $(dirname $0)
 
+# Let's make sure the manifest is up to date.
+sh _build-manifest.sh
+
 kubectl create secret generic -n media plex-claim-token --from-file=token=plex-claim --dry-run=client -o yaml | kubectl apply -f -
+
+# Upload the authbind password to the infrastructure namespace.
+kubectl create secret generic -n infrastructure ldap-authbind --from-file=password=ldap-authbind --dry-run=client -o yaml | kubectl apply -f -
