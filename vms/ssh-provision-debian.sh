cecebe7b882edab17544addc28b8fb1448a34aeb
Guanyu Wu <lambda@funcptr.org>
2020-12-15 13:51:01 -0500

    Whoops, was in subdirectory. This is the actual manifest.

--- vms/ssh-provision-debian.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo -n "Password for debian@$1:"
-read -s PASS
-
-cd $(dirname $0)
-ssh-copy-id -i ../secrets/ansible debian@$1
-ssh -i ../secrets/ansible debian@$1 $(echo "echo \"$PASS\" | sudo -S sh -c \"echo 'debian ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/custom\"")
-ssh -i ../secrets/ansible debian@$1 "sudo passwd -d debian"
