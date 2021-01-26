3aa6ae72fb979d3c6989597379223b3bce791137
Guanyu Wu <lambda@funcptr.org>
2021-01-26 15:37:49 -0500

    Added ifupdown2 install and reboot to debian provisioning script.

--- vm/provisioning/ssh-provision-debian.sh
+++ vm/provisioning/ssh-provision-debian.sh
@@ -9,3 +9,4 @@ cd $(dirname $0)
 ssh-copy-id -i ../../secrets/ansible debian@$1
 ssh -i ../../secrets/ansible debian@$1 $(echo "echo \"$PASS\" | sudo -S sh -c \"echo 'debian ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/custom\"")
 ssh -i ../../secrets/ansible debian@$1 "sudo passwd -d debian"
+ssh -i ../../secrets/ansible debian@$1 "apt update; apt install ifupdown2; reboot"
