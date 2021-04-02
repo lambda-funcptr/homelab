3d2a7e693d59a0da1cc1ff67d558233e541f3270
Guanyu Wu <lambda@funcptr.org>
2021-04-01 22:38:29 -0400

    Updated headers on site.

--- cloud/wgcdn/bin/fetchcerts.sh
+++ cloud/wgcdn/bin/fetchcerts.sh
@@ -6,6 +6,6 @@ while read -r DOMAIN_ARGS; do
     if [ "$DOMAIN_ARGS" != "---" ]; then
         certbot certonly --standalone --non-interactive --keep-until-expiring --renew-with-new-domains \
             --pre-hook "systemctl stop lighttpd" --post-hook "systemctl start lighttpd; systemctl reload nginx" \
-            --agree-tos --register-unsafely-without-email $DOMAIN_ARGS
+            --agree-tos --register-unsafely-without-email $DOMAIN_ARGS -d $(hostname -f)
     fi
 done < /srv/certs.cfg
